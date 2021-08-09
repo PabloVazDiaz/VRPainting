4a47be65b161afa037b182c037e9354ef6376680
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2021-08-09 10:03:19 +0200

    Project Init

---
+++ Source/VRPainting/VRPaintingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRPaintingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPAINTING_API AVRPaintingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
